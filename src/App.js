07d853f52e6eefa39e512cbd697c2f8abb3a2160
Caleb Dubow <cdubow@SBT.local>
2022-06-08 17:52:31 -0400

    Removed direct-chat path

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Inbox from "./components/Inbox";
 import NavigationBar from "./components/NavigationBar";
 import Home from "./components/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import * as module from "https://t2c-stage.solutionsbytext.com/js/interactions/module.js";
 module.loadJSFiles();
 
@@ -14,18 +15,6 @@ function App() {
         <NavigationBar />
         <Routes>
           <Route path="/" element={<Home />} exact />
-          <Route
-            path="/direct-chat"
-            element={
-              <Chatbox
-                messageType="Twoway"
-                accountId="271759e9-ca4f-4835-be61-0fe38d051559"
-                groupId="425fa869-6116-4e0c-af1c-0c764e78bc70"
-                sbtOrgunitId="b85c02b4-c3de-4c7f-8717-97aa551652bb"
-                subscriberNo="16094805359"
-              />
-            }
-          />
           <Route
             path="/inbox"
             element={
